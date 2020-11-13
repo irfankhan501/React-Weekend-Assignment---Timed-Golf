b317691e3706d408a714a3bbb80583762a6a8525
Irfan Khan <irfankhan501@outlook.com>
2020-11-13 09:40:25 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,62 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { time: 0, x: 0, y: 0 };
+    this.intervalID = null;
   }
-  componentDidMount() {
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidUpdate(prevProps, prevState) {
+    document.removeEventListener("keydown", this.handlekeyPressed.bind(this));
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.intervalID);
+    }
     
   }
 
-  componentWillUnmount() {
-    
+  // handleRemove() {
+  //   console.log("remove");
+  // }
+
+  handleClick() {
+    this.intervalID = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+    document.addEventListener("keydown", this.handlekeyPressed.bind(this));
   }
 
-
+  handlekeyPressed(e) {
+    // console.log(e);
+    const X = this.state.x;
+    const Y = this.state.y;
+    if (e.keyCode === 39) {
+      this.setState({ x: X + 5 });
+    }
+    if (e.keyCode === 37) {
+      this.setState({ x: X - 5 });
+    }
+    if (e.keyCode === 40) {
+      this.setState({ y: Y + 5 });
+    }
+    if (e.keyCode === 38) {
+      this.setState({ y: Y - 5 });
+    }
+  }
 
   render() {
     return (
- <>
-</>
+      <>
+        <p className="heading-timer">Timer = {this.state.time}</p>
+        <button className="start" onClick={this.handleClick.bind(this)}>
+          Start
+        </button>
+        <div
+          className="ball"
+          style={{ left: this.state.x, top: this.state.y }}
+        ></div>
+        <div className="hole"></div>
+      </>
     );
   }
 }
